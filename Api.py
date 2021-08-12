391fb5b1d5e39894ac8d814b8bbdff4595969f3d
Tina Lenk <lenk.tina@gmx.de>
2021-08-12 11:56:11 +0200

    add statusCode

--- Api.py
+++ Api.py
@@ -7,8 +7,13 @@ def index():
     return "<h1>Hello, World!</h1>"
 def app(environment, startResponse):
     data = (b"No data here\n")
-    statu = "200 OK"
-
+    status = "200 OK"
+    responseHeaders = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    startResponse(status, responseHeaders)
+    return iter([data])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0:5000')
