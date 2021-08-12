036bce5cd1dfab1214de19943f11b1d02c3334d4
Tina Lenk <lenk.tina@gmx.de>
2021-08-12 12:52:16 +0200

    try API stuff

--- app/Api.py
+++ app/Api.py
@@ -2,10 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "<h1>Hello, World!</h1>"
-#def app(environment, startResponse):
+
+@app.route("/api/v1/plant", method=["GET"])
+def getPlant():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "ERROR: No ID field provided. Specify an ID!"
+
+    results = [42]
+
+#    for plant in plants:
+ #       if plant['id'] == id:
+  #          results.append(plant)
+
+    return jsonify(results)
+
+# def app(environment, startResponse):
 #    data = (b"No data here\n")
 #    status = "200 OK"
 #    responseHeaders = [
