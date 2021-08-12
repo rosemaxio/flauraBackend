826b81c05cce4ebbffe3e2a7e2b882983dd31ed8
Max Rose <max.rose@audory.io>
2021-08-13 01:16:22 +0200

    added read/write

--- test/Api.py
+++ test/Api.py
@@ -1,7 +1,16 @@
 from . import test
+from flask import Flask, request, Response
 
 #@test.route('/test/<accessToken>/<method>/<attr>') # plus params ?value=3
 @test.route('/test/<accessToken>/<method>/<attr>')
 def get_test_value(accessToken, method, attr):
-    print(accessToken, method, attr)
-    return '42'
+    # print(accessToken, method, attr)
+    if method == "get":
+        print('\nLESEN\n')
+        return '42'
+    elif method == "update":
+        value = request.args.get('value')
+        print('\nSCHREIBEN\n', value, '\n\n')
+        return ''
+    
+    return ''
