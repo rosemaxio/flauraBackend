ed2967e80e6c3c7f6468656eca8a2985d4ed744d
Tina Lenk <lenk.tina@gmx.de>
2021-08-13 12:48:56 +0200

    API for arduino - server should work. No security/safety stuff in place

--- test/Api.py
+++ test/Api.py
@@ -1,16 +1,15 @@
-from . import test
+from . import test, db
 from flask import Flask, request, Response
 
-#@test.route('/test/<accessToken>/<method>/<attr>') # plus params ?value=3
-@test.route('/test/<accessToken>/<method>/<attr>')
+
+# @test.route('/test/<accessToken>/<method>/<attr>') # plus params ?value=3
+@test.route('/test/<accessToken>/<method>/<attr>', methods=["GET"])
 def get_test_value(accessToken, method, attr):
     # print(accessToken, method, attr)
     if method == "get":
-        print('\nLESEN\n')
-        return '42'
+        return db.getTestValue(accessToken, attr)
     elif method == "update":
-        value = request.args.get('value')
-        print('\nSCHREIBEN\n', value, '\n\n')
-        return ''
-
-    return ''
+        if 'value' in request.args:
+            value = request.args.get('value', type=float, default=-1)
+            return db.updateTestValue(accessToken, attr, value)
+    return 'Something went wrong... Have you checked if the URL is correct?'
--- test/db.py
+++ test/db.py
@@ -1,20 +1,54 @@
 from dotenv import load_dotenv
-# from flask import jsonify
+import os
+from flask import request, Response
+from bson.json_util import dumps
+import json
 import pymongo
-'''
+
 load_dotenv()  # use dotenv to hide sensitive credential as environment variables
 DATABASE_URL = f'mongodb+srv://{os.environ.get("dbUser")}:{os.environ.get("dbPasswort")}' \
                '@flask-mongodb-atlas.wicsm.mongodb.net/' \
                'flaura?retryWrites=true&w=majority'  # get connection url from environment
 
 client = pymongo.MongoClient(DATABASE_URL)  # establish connection with database
-'''
-# plants.config['MONGO_DBNAME'] = 'restdb'
-# plants.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
-# mongo = PyMongo(plants)
 
 mydb = client.flaura
 mycol = mydb.users
+
+
+def jsonResponse(data):
+    return Response(data, mimetype='application/json')
+
+
+def getUserByPlantToken(token):
+    susUser = mycol.find_one({"pots.token": token})
+    if susUser == None:
+        return "noUser"
+    else:
+        return susUser
+
+
+def updateTestValue(token, name, newValue):
+    newData = getUserByPlantToken(token)
+    if newData != "noUser":
+        for pot in newData["pots"]:
+            if pot["token"] == token:
+                pot[name] = newValue;
+        mycol.save(newData) # could probably also use update
+        return 'success'
+    return "Something went wrong... Is the Token set correctly?"
+
+def getTestValue(token, name):
+    newData = getUserByPlantToken(token)
+    if newData != "noUser":
+        for pot in newData["pots"]:
+            if pot["token"] == token:
+                return jsonResponse(dumps(pot[name]))
+    return "Something went wrong... Is the Token set correctly?"
+# getCorrect Person entry via current Token
+# then find the right plant via plant token
+# then get the value there
+# and change it
 '''
 pipeline = [
     { $filter: {
